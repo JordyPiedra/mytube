8a16bd268fc6516256959ecca5f7c8256587fa8c
JPIEDRA-DTI <dsoftwarea@pucesd.edu.ec>
2017-03-15 18:00:18 -0500

    Login part 1

--- frontEnd/src/app/components/login/login.component.ts
+++ frontEnd/src/app/components/login/login.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+ public user:any;
   constructor() { }
 
   ngOnInit() {
+    this.user={
+      useremail:"",
+      userpassword:"",
+      usergetHash :false
+    };
   }
 
 }
